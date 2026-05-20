b44f7b963cbb7ffe109a05d789d45fba7ca0f33c
ajinathtangade12@gmail.com <ajinathtangade12@gmail.com>
2026-05-20 23:12:50 +0530

    Connected installer routes

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const installerRoutes = require('./routes/installerRoutes');
+
 const app = express();
 
 app.use(cors({
-  origin: 'https://new-shoe-brand.myshopify.com/'
+  origin: 'https://new-shoe-brand.myshopify.com'
 }));
+
 app.use(express.json());
+
+app.use('/api/installers', installerRoutes);
 
 app.get('/', (req, res) => {
   res.send('OZEV API Running');
